ca3bb53fc76cb55b0682e423215e039cfd819734
Praveen Singampalli <praveensingampalli1994@gmail.com>
2023-03-18 09:20:07 +0530

    Update SpringbootApplicationTests.java

--- src/test/java/com/example/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/example/springboot/SpringbootApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.springboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@ActiveProfiles("dev")
+
 @SpringBootTest
 class SpringbootApplicationTests {
 
